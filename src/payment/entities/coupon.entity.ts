da12be3d2381c1bfbae410ea02b46f56d55687ff
muyeon <gmakin36@gmail.com>
2024-03-13 01:54:53 +0900

    feat: add coupon entity
    - 쿠폰 엔티티 추가
    - 발급된 쿠폰 엔티티를 만들면 관계 설정 때문에 변경필요
    - 관리자만 만들 수 있도록 만들자

---
+++ src/payment/entities/coupon.entity.ts
@@ -0,0 +1,21 @@
+import { BaseEntity, Column } from 'typeorm';
+
+export enum CouponType {
+  percent = 'percent', // 정률제
+  fixed = 'fixed', // 정액제
+}
+
+export class CouponModel extends BaseEntity {
+  @Column({
+    type: 'enum',
+    enum: CouponType,
+  })
+  type: CouponType;
+
+  @Column({
+    type: 'decimal',
+    precision: 5,
+    scale: 2,
+  })
+  value: number; // 얼마나 할인 되는지
+}
