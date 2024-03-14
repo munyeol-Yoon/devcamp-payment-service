634ac634580f77c5618e13be15dc7cc19c5a9e6a
muyeon <gmakin36@gmail.com>
2024-03-15 00:48:13 +0900

    feat: add product entity
    - 상품 저장 및 생성 등을 위한 product 엔티티 생성

---
+++ src/payment/entities/product.entity.ts
@@ -0,0 +1,41 @@
+import { BaseEntity, Column, Entity } from 'typeorm';
+
+export type ProductStatus = 'available' | 'out-of-stock';
+
+@Entity('product')
+export class ProductModel extends BaseEntity {
+  @Column({
+    type: 'varchar',
+  })
+  name: string;
+
+  @Column({
+    type: 'decimal',
+    precision: 10,
+    scale: 2,
+  })
+  price: number;
+
+  @Column({
+    type: 'int',
+    default: 0,
+  })
+  stock: number;
+
+  @Column({
+    type: 'varchar',
+    nullable: true,
+  })
+  description: string;
+
+  @Column({
+    type: 'varchar',
+    default: 'available',
+  })
+  status: ProductStatus;
+
+  // 추후 사용할떄 추가하자
+  // category: string;
+
+  // imageUrl: string;
+}
